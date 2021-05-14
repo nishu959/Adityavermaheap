674b3247f29eccb53679c3de28020272adf2e52b
nishu959 <60173728+nishu959@users.noreply.github.com>
2021-05-14 10:51:45 +0530

    Add files via upload

---
+++ topkfrequentelement.py
@@ -0,0 +1,26 @@
+a = list(map(int, input().split()))
+k = int(input())
+
+h = {}
+for i in a:
+  if i in h:
+    h[i] = h[i]+1
+  else:
+    h[i] = 1
+  
+from heapq import heappop, heappush, heapify
+t = []
+heapify(t)
+for i in h:
+  heappush(t, (h[i], i))
+ 
+  if len(t)>k:
+    heappop(t)
+  
+
+
+for i, j in t:
+  print(j, end = " ")
+  
+  
+ 
